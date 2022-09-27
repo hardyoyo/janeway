19bd2a61e4304665c3e4778b9fb0d8a1540c15bb
Hardy Pottinger <hardy.pottinger@gmail.com>
2022-09-27 13:58:18 -0500

    Add DRF pagination to janeway_global_settings

--- src/core/janeway_global_settings.py
+++ src/core/janeway_global_settings.py
@@ -557,7 +557,6 @@ if ENABLE_OIDC:
         'django.contrib.auth.backends.ModelBackend',
     )
 
-    
 CORE_FILETEXT_MODEL = "core.FileText"
 if os.environ.get("DB_VENDOR") == "postgres":
     CORE_FILETEXT_MODEL = "core.PGFileText"
@@ -571,3 +570,9 @@ CORE_THEMES = [
     'clean',
 ]
 INSTALLATION_BASE_THEME = 'OLH'
+
+# Use pagination for all of our APIs based on Django REST Framework
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 100
+}
